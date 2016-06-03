d6f89e12fa9b7ce9b67115fb6efedb4d200056c4
h4w5 <pj@ga.co>
2016-06-03 11:35:27 -0700

    add User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+
+# user = User.new
+# user.password = "1234"
+# user.password_confirmation = "1234"
+
+pj = User.new(
+  email:                 "pj@ga.co",
+  password:              "1234",
+  password_confirmation: "1234",
+)
+pj.save
+
+ez = User.new(
+  email:                 "ez@ga.co",
+  password:              "1234",
+  password_confirmation: "1234",
+  handle:                "ezrazilla",
+  profile_url:           "https://ga-core.s3.amazonaws.com/production/uploads/instructor/image/6752/thumb_harsh_ez.jpg",
+  statement:             "We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defense, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America."
+)
+ez.save
